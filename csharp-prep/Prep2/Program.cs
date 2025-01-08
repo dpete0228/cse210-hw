7e9d84b12008b1bb3ea92c7e1d23c0474a3e869e
David Peters <pet24046@byui.edu>
2025-01-08 14:23:34 -0700

    Added and changed prep2 to complete the assignment requirements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Welcome to my letter grade calculator");
+        Console.Write("Please enter the percentage that you received (Without the % sign): ");
+        string userInput = Console.ReadLine();
+        int percentGrade = int.Parse(userInput);
+        string letterGrade = "";
+        if (percentGrade >= 90){
+            letterGrade = "A";
+        }else if (percentGrade >= 80){
+            letterGrade = "B";
+        }else if (percentGrade >= 70){
+            letterGrade = "C";
+        }else if (percentGrade >= 60){
+            letterGrade = "D";
+        }else if (percentGrade < 60){
+            letterGrade = "F";
+        }
+        Console.WriteLine($"You got a(n) {letterGrade} in the class");
+
     }
 }
