7f2e2c369abc7af5ec6e69c00aaad6b87ec0c0b0
David Peters <pet24046@byui.edu>
2025-01-14 11:51:26 -0700

    Wrote prep3 to the requirements. Generates a random number and then loops and asks for guesses and directs the user to it.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+        int guess;
+        do{
+            Console.Write("What is your guess? ");
+            string userInput2 = Console.ReadLine();
+            guess = int.Parse(userInput2);
+            if (guess == magicNumber){
+                Console.WriteLine("You guessed it!");
+            }else if(guess < magicNumber){
+                Console.WriteLine("Higher");
+            }else{
+                Console.WriteLine("Lower");
+            }
+        }while(magicNumber != guess);  
     }
 }
