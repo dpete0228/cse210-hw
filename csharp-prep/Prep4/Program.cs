64dba2a0e8c565119fa5896e13c6381ac65c2787
David Peters <pet24046@byui.edu>
2025-01-14 12:16:48 -0700

    Changed Prep 4 and added in the code to have it get a list of numbers and hten output hte correct numbers

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,33 @@
 using System;
-
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+        int userNumber;
+        do {
+            Console.Write("Enter a number: ");
+            string userInput = Console.ReadLine();
+            userNumber = int.Parse(userInput);
+            numbers.Add(userNumber);
+        }while(userNumber != 0);
+        int sum = 0;
+        int largestNumber = 0;
+        int count = numbers.Count();
+        foreach(int number in numbers){
+            sum += number;
+            if(largestNumber < number){
+                largestNumber = number;
+            }
+        }
+        count -= 1;
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine(count);
+        int average = sum / count;
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largestNumber}");
     }
 }
