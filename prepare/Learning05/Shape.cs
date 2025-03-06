cdae3f6c9578e39bc2b2fb475a3cbd65a778381b
David Peters <pet24046@byui.edu>
2025-03-06 11:43:05 -0700

    Modified learnign 5 to meet the requirements

---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,17 @@
+using System;
+
+public abstract class Shape
+{
+    public Shape(string color){
+        _color = color;
+    }
+    private string _color;
+
+    public string GetColor(){
+        return _color;
+    }
+    public void SetColor(string color){
+        _color = color;
+    }
+    public abstract double GetArea();
+}
